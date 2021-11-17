7eacef53d8a76c3d52ea1f132df5cadc72ab4f3e
sofia-papa <papa.sofia@outlook.it>
2021-11-17 15:54:10 +0000

    create migration

--- database/migrations/2021_11_17_084422_create_categories_table.php
+++ database/migrations/2021_11_17_084422_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 30)->unique();
+            $table->string('slug', 40)->unique();
             $table->timestamps();
         });
     }
